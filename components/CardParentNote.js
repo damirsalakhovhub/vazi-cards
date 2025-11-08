942ee339af88516f9432743a85e02a1b30832c54
Damir Salakhov <damirsalakhov@gmail.com>
2025-11-08 21:17:35 +0500

    Add plus icon on hover to parent note block

--- components/CardParentNote.js
+++ components/CardParentNote.js
@@ -1,3 +1,5 @@
+const parentNotePlusSvg = '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="Property 1=Medium"><path id="Icon" fill-rule="evenodd" clip-rule="evenodd" d="M10 4C10.5523 4 11 4.44772 11 5V9H15C15.5523 9 16 9.44772 16 10C16 10.5523 15.5523 11 15 11H11V15C11 15.5523 10.5523 16 10 16C9.44772 16 9 15.5523 9 15V11H5C4.44772 11 4 10.5523 4 10C4 9.44772 4.44772 9 5 9H9V5C9 4.44772 9.44772 4 10 4Z" fill="currentColor"/></g></svg>';
+
 export function createCardParentNote(noteData) {
   if (!noteData) {
     return null;
@@ -11,6 +13,11 @@ export function createCardParentNote(noteData) {
   text.textContent = noteData.text;
   
   note.appendChild(text);
+  
+  const plus = document.createElement('div');
+  plus.className = 'card-parent-note-plus';
+  plus.innerHTML = parentNotePlusSvg;
+  note.appendChild(plus);
   
   if (noteData.icon) {
     const icon = document.createElement('span');
