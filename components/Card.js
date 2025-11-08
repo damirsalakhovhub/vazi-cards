8941666e9df585949135aa29454a97f781ea58b7
Damir Salakhov <damirsalakhov@gmail.com>
2025-11-08 21:18:16 +0500

    Add menu button with three dots on card hover

--- components/Card.js
+++ components/Card.js
@@ -6,10 +6,18 @@ import { createCardDate } from './CardDate.js';
 import { createCardSubtasks } from './CardSubtasks.js';
 import { createCardParentNote } from './CardParentNote.js';
 
+const dotsMenuSvg = '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.5 10C3.5 9.17157 4.17157 8.5 5 8.5C5.82843 8.5 6.5 9.17157 6.5 10C6.5 10.8284 5.82843 11.5 5 11.5C4.17157 11.5 3.5 10.8284 3.5 10Z" fill="currentColor"/><path d="M8.5 10C8.5 9.17157 9.17157 8.5 10 8.5C10.8284 8.5 11.5 9.17157 11.5 10C11.5 10.8284 10.8284 11.5 10 11.5C9.17157 11.5 8.5 10.8284 8.5 10Z" fill="currentColor"/><path d="M15 8.5C14.1716 8.5 13.5 9.17157 13.5 10C13.5 10.8284 14.1716 11.5 15 11.5C15.8284 11.5 16.5 10.8284 16.5 10C16.5 9.17157 15.8284 8.5 15 8.5Z" fill="currentColor"/></svg>';
+
 export function createCard(cardData, allCards = []) {
   const card = document.createElement('div');
   card.className = 'card';
   card.setAttribute('data-card-id', cardData.id);
+  
+  const menuButton = document.createElement('button');
+  menuButton.className = 'card-menu-button';
+  menuButton.type = 'button';
+  menuButton.innerHTML = dotsMenuSvg;
+  card.appendChild(menuButton);
   
   const main = document.createElement('div');
   main.className = 'card-main';
