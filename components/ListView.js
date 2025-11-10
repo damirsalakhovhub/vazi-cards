b2e08250d12ff5867fca21a75ca72dcf3ad8eaf9
Damir Salakhov <damirsalakhov@gmail.com>
2025-11-10 16:31:04 +0500

    Add checkboxes and assignee column with avatars to list view

--- components/ListView.js
+++ components/ListView.js
@@ -1,3 +1,7 @@
+import { createCardAvatars } from './CardAvatars.js';
+
+const checkboxCheckedSvg = '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.5677 6.64017L9.56771 14.6402C9.19673 15.0853 8.50214 15.1168 8.09238 14.7071L4.09238 10.7071C3.18043 9.79514 4.59464 8.38093 5.50659 9.29288L8.73229 12.5186L15.0313 5.3598C15.8569 4.36903 17.3933 5.6494 16.5677 6.64017Z" fill="#ffffff"/></svg>';
+
 function groupCardsByColumn(cards, columns) {
   const grouped = {};
   
@@ -100,8 +104,19 @@ export function createListView(boardData) {
       const taskCell = document.createElement('div');
       taskCell.className = 'list-view-cell list-view-cell-task';
       
-      const checkbox = document.createElement('div');
-      checkbox.className = 'list-view-checkbox';
+      const checkboxWrapper = document.createElement('label');
+      checkboxWrapper.className = 'checkbox-wrapper';
+      
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.className = 'checkbox';
+      
+      const checkboxIcon = document.createElement('span');
+      checkboxIcon.className = 'checkbox-icon';
+      checkboxIcon.innerHTML = checkboxCheckedSvg;
+      
+      checkboxWrapper.appendChild(checkbox);
+      checkboxWrapper.appendChild(checkboxIcon);
       
       const taskContent = document.createElement('div');
       taskContent.className = 'list-view-task-content';
@@ -116,11 +131,14 @@ export function createListView(boardData) {
       
       taskContent.appendChild(id);
       taskContent.appendChild(title);
-      taskCell.appendChild(checkbox);
+      taskCell.appendChild(checkboxWrapper);
       taskCell.appendChild(taskContent);
       
       const assigneeCell = document.createElement('div');
       assigneeCell.className = 'list-view-cell list-view-cell-assignee';
+      
+      const avatars = createCardAvatars(card.assignees);
+      assigneeCell.appendChild(avatars);
       
       const dateCell = document.createElement('div');
       dateCell.className = 'list-view-cell list-view-cell-date';
